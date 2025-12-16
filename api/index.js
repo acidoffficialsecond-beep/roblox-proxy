040883c4923611015d308bcaf5ba6fd731b5674e
acidoffficialsecond-beep <acidoffficialsecond@gmail.com>
2025-12-16 17:32:11 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
   res.status(200).json({
-    message: "Roblox Proxy API is running",
+    message: "Pro Sigma Pro, USE THESE NOOB: ",
     usage: {
       gamePasses: "/api/game-passes?gameId=GAME_ID",
       userGames: "/api/user-games?userId=USER_ID"
